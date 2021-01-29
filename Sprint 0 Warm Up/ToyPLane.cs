b02a60acbe06eb44bfa9c03b83537b2ef4030f31
MaxwellJFleming <71738689+MaxwellJFleming@users.noreply.github.com>
2021-01-28 22:45:50 -0500

    finished

--- Sprint 0 Warm Up/ToyPLane.cs
+++ Sprint 0 Warm Up/ToyPLane.cs
@@ -12,5 +12,34 @@ namespace Sprint_0_Warm_Up
         {
             MaxAltitude = 50;
         }
+
+        public string getWindUpString()
+        {
+            if (isWoundUp)
+                return "String is wound up.";
+            else
+                return "String is not wound up.";
+        }
+
+        public void StartEngine()
+        {
+            if (isWoundUp)
+                Engine.Start();
+        }
+
+        public string TakeOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Unwind()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WindUp()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
